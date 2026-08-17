a1a7cf58c5436b4ab13ff0ab6643e9196beb0d4d
Karol Błażejczyk <blazkaro@proton.me>
2026-08-17 17:33:28 +0200

    compaction trigger, excluded from previous commit by mistake

---
+++ src/compaction/trigger.rs
@@ -0,0 +1,23 @@
+use glommio::channels::local_channel::{LocalReceiver, LocalSender};
+use std::rc::Rc;
+
+#[derive(Clone)]
+pub struct CompactionTrigger {
+    sender: Rc<LocalSender<()>>,
+}
+
+impl CompactionTrigger {
+    pub fn new() -> (Self, LocalReceiver<()>) {
+        let (sender, receiver) = glommio::channels::local_channel::new_bounded(1);
+        (
+            Self {
+                sender: Rc::new(sender),
+            },
+            receiver,
+        )
+    }
+
+    pub fn notify(&self) {
+        let _ = self.sender.try_send(());
+    }
+}
